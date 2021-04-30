f4ab2e36c557752617445d32d5a6fca6fdd1c427
Kevin Araneda Otero <42554629+IronGreenK@users.noreply.github.com>
2021-04-29 23:28:56 -0400

    Add files via upload

---
+++ Tarea Array/js/script.js
@@ -0,0 +1,66 @@
+var bolsita = [];
+
+function producto1(){
+    var producto1 = ["Frostpunk",15000];
+    bolsita.push(producto1);
+}
+
+function producto2(){
+    var producto2 = ["GTA",25000];
+    bolsita.push(producto2);
+}
+
+function producto3(){
+    var producto3 = ["Resident Evil 8",59000];
+    bolsita.push(producto3);
+}
+
+function producto4(){
+    var producto4 = ["Monster Hunter",35000];
+    bolsita.push(producto4);
+}
+
+function producto5(){
+    var producto5 = ["Battlefield",45000];
+    bolsita.push(producto5);
+}
+
+function producto6(){
+    var producto6 = ["Mortal Kombat 11",20000];
+    bolsita.push(producto6);
+}
+
+function producto7(){
+    var producto7 = ["Stardew Valley",7000];
+    bolsita.push(producto7);
+}
+
+function producto8(){
+    var producto8 = ["Satisfactory",17000];
+    bolsita.push(producto8);
+}
+
+function producto9(){
+    var producto9 = ["Call of duty Cold War",15000];
+    bolsita.push(producto9);
+}
+
+function producto10(){
+    var producto10 = ["Minecraft",20000];
+    bolsita.push(producto10);
+}
+
+function producto11(){
+    var producto11 = ["Phasmophobia",7000];
+    bolsita.push(producto11);
+}
+
+function producto12(){
+    var producto12 = ["Valheim",8000];
+    bolsita.push(producto12);
+}
+
+function finish(){
+    console.log(bolsita);
+    alert(bolsita)
+}
